b86d46af7e29ecead5d774481f2feda9bca679a9
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-01 14:04:04 +0200

    updates submodules sh script

--- src/scripts/git-submodules-init.sh
+++ src/scripts/git-submodules-init.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd ${SCRIPT_DIR}/.. \
-&& git submodule update --init --recursive --verbose \
+&& git submodule update --init --recursive --recommend-shallow --single-branch --verbose \
+&& git submodules status \
 && popd
 
