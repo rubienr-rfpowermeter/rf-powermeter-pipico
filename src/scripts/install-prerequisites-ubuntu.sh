1acd490ebe7504466dc613a72cc6837abd2f2e9d
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-01 14:27:39 +0200

    updates workflow

---
+++ src/scripts/install-prerequisites-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
