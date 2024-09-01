0233dd8cdf989c17ccbe6d6db5748c0de6e87752
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-01 15:16:51 +0200

    updates workflow

--- src/scripts/git-submodule-init.sh
+++ src/scripts/git-submodule-init.sh
@@ -3,7 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd ${SCRIPT_DIR}/.. \
-&& git submodule update --init --recursive --recommend-shallow --single-branch --verbose \
+&& git submodule update --init --recursive
+&& git submodule update --recursive --recommend-shallow --single-branch --verbose \
 && git submodules status \
 && popd
 
