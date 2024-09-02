8d179a8a25d2d66f053a3d99c9d561a0503c3ccb
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-02 21:33:11 +0200

    cleanup

--- src/scripts/install-prerequisites-ubuntu.sh
+++ src/scripts/install-prerequisites-ubuntu.sh
@@ -2,4 +2,9 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+sudo apt install \
+  cmake \
+  gcc-arm-none-eabi \
+  libnewlib-arm-none-eabi \
+  libstdc++-arm-none-eabi-newlib \
+
