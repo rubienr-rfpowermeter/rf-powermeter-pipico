650f0e688f2590fbabcd990326eebb715aa2a064
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-03 02:36:57 +0200

    introduces table-view + info page

--- src/modules/main_ui/main.cpp
+++ src/modules/main_ui/main.cpp
@@ -8,8 +8,10 @@
 
 #include "demos/keypad_encoder/lv_demo_keypad_encoder.h"
 #include "lvgl.h"
-
+#include "modules/globals/globals.h"
 #include "pico/stdlib.h"
+
+#include <cstdio>
 
 extern lv_img_dsc_t ai;
 lv_obj_t *img1 = nullptr;
@@ -178,11 +180,96 @@ void lv_example_btn_1()
 
 static void unused_cb(uint __unused gpio, uint32_t __unused event_mask) {}
 
-[[noreturn]]
-void main_core0()
+void default_tab_view()
 {
-    stdio_init_all();
+    lv_obj_t *tabview;
+    tabview = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, 50);
+
+    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "Meter");
+    lv_obj_t *tab2 = lv_tabview_add_tab(tabview, "Config");
+    lv_obj_t *tab3 = lv_tabview_add_tab(tabview, "Info");
+
+    lv_obj_t *label = lv_label_create(tab1);
+    lv_label_set_text(label, "This the first tab\n\n"
+                             "If the content\n"
+                             "of a tab\n"
+                             "becomes too\n"
+                             "longer\n"
+                             "than the\n"
+                             "container\n"
+                             "then it\n"
+                             "automatically\n"
+                             "becomes\n"
+                             "scrollable.\n"
+                             "\n"
+                             "\n"
+                             "\n"
+                             "Can you see it\nx\nx\nx\nx\nx?");
+
+    label = lv_label_create(tab2);
+    lv_label_set_text(label, "Second tab");
+
+
+    static lv_coord_t col_dsc[] = {200, 240, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc[] = {400, LV_GRID_TEMPLATE_LAST};
+    lv_obj_set_style_grid_column_dsc_array(tab3, col_dsc, 0);
+    lv_obj_set_style_grid_row_dsc_array(tab3, row_dsc, 0);
+    // lv_obj_set_size(tab3, 480, 500);
+    lv_obj_center(tab3);
+    lv_obj_set_layout(tab3, LV_LAYOUT_GRID);
+
+    globals.display.touch.product_id = 0xabcdef0a;
+
+    label = lv_label_create(tab3);
+    char product_id[512] = {0};
+
+
+    snprintf(product_id, sizeof(product_id),
+             "Display :\n"
+             " Product ID:  \t 0x%08" PRIx32 "\n"
+             " Resolution:  \t %" PRIu16 " x %" PRIu16 "\n"
+             " I2C Address: \t0x%" PRIx8 "\n"
+             " Baud Rate:   \t %" PRIu32 "\n"
+             " FW Version:  \t0x%08" PRIx16 "\n",
+             globals.display.touch.product_id, globals.display.touch.width_px, globals.display.touch.height_px,
+             globals.display.touch.i2c_address, globals.display.touch.baud_rate, globals.display.touch.firmware_version);
+
+    lv_label_set_text(label, product_id);
+    lv_obj_set_grid_cell(label, LV_GRID_ALIGN_STRETCH, 0, 1, LV_GRID_ALIGN_STRETCH, 0, 1);
+
+    lv_obj_scroll_to_view_recursive(label, LV_ANIM_ON);
+
+    const uint8_t rom_version = {rp2040_rom_version()};
+    label = lv_label_create(tab3);
+    snprintf(product_id, sizeof(product_id),
+             "Firmware: \n"
+             " Version:      \t\t" PICO_PROGRAM_VERSION_STRING " \n"
+             " Build Date:  \t" __DATE__ " \n"
+             " Description:\t" PICO_PROGRAM_DESCRIPTION "\n"
+             " Binary:     \t\t\t" PICO_PROGRAM_NAME " \n"
+             " Build Type:  \t" PICO_CMAKE_BUILD_TYPE " \n"
+             " Board:      \t\t\t" PICO_BOARD "\n"
+             " Rom V.:      \t\t%" PRIu8 "\n"
+             " pico-sdk V.: \t" PICO_SDK_VERSION_STRING " \n"
+             " URL: " PICO_PROGRAM_URL "\n", rom_version);
+    lv_label_set_text(label, product_id);
+    lv_obj_set_grid_cell(label, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_STRETCH, 0, 1);
+}
+
+void example()
+{
+    img1 = lv_img_create(lv_scr_act());
+    lv_img_set_src(img1, &ai);
+    lv_obj_align(img1, LV_ALIGN_DEFAULT, 0, 0);
+    lv_example_btn_1();
+}
+
+static void init()
+{
     setup_default_uart();
+    printf("main_core0: init ...\n");
+
+    stdio_init_all();
 
     lv_init();
     lv_port_disp_init();
@@ -190,6 +277,13 @@ void main_core0()
     rgbw_init();
 
     joystick_init(unused_cb);
+    printf("main_core0: init done\n");
+}
+
+[[noreturn]]
+void main_core0()
+{
+    init();
 
     repeating_timer_t timer;
     add_repeating_timer_ms(1, ms_tick_timer_cb, nullptr, &timer);
@@ -197,12 +291,10 @@ void main_core0()
     lv_obj_clean(lv_scr_act());
     busy_wait_ms(100);
 
-    img1 = lv_img_create(lv_scr_act());
-    lv_img_set_src(img1, &ai);
-    lv_obj_align(img1, LV_ALIGN_DEFAULT, 0, 0);
-    lv_example_btn_1();
+    default_tab_view();
+    // example();
 
-    while(1)
+    while(true)
     {
         if(0 == systemTicksMs % 5) lv_task_handler();
     }
