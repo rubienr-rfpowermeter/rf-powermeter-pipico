ea89beb7cceca1faf52077454a00e46a3c297c69
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-09 19:51:23 +0200

    refactors struct for lazy casting

--- src/modules/globals/cast.h
+++ src/modules/globals/cast.h
@@ -2,51 +2,46 @@
 
 #include <stdint.h>
 
+typedef union __packed
+{
+  uint32_t int32;
+  int32_t uint32;
+} As32BitView;
+
+typedef union __packed
+{
+  int16_t int16;
+  uint16_t uint16;
+} As16BitView;
+
+typedef struct __packed
+{
+  As16BitView l;
+  As16BitView h;
+
+} As2x16BitView;
+
+typedef union __packed
+{
+  int8_t int8;
+  uint8_t uint8;
+} As8BitView;
+
+typedef struct __packed
+{
+  As8BitView b0;
+  As8BitView b1;
+  As8BitView b2;
+  As8BitView b3;
+} As4x8BitView;
+
+//! 32-bit type for lazy casts
 typedef union
 {
-    union
-    {
-        uint32_t int32;
-        int32_t uint32;
-    } as_32bit;
-
-    struct
-    {
-        union
-        {
-            int16_t int16;
-            uint16_t uint16;
-        } l;
-        union
-        {
-            int16_t int16;
-            uint16_t uint16;
-        } h;
-
-    } __packed as_16bit;
-
-    struct
-    {
-        union
-        {
-            int8_t int8;
-            uint8_t uint8;
-        } b0;
-        union
-        {
-            int8_t int8;
-            uint8_t uint8;
-        } b1;
-        union
-        {
-            int8_t int8;
-            uint8_t uint8;
-        } b2;
-        union
-        {
-            int8_t int8;
-            uint8_t uint8;
-        } b3;
-    } __packed as_8bit;
-
+  As32BitView as_32bit;
+  As2x16BitView as_16bit;
+  As4x8BitView as_8bit;
+  float as_float;
 } IntCast;
+
+static_assert(sizeof(IntCast) == 4, "error: size mismatch");
