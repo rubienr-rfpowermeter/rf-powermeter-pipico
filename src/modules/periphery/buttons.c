a76935e1491c10268377e3c9efcd64766930bc19
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-09-15 00:34:13 +0200

    buttons: fixes initialization

--- src/modules/periphery/buttons.c
+++ src/modules/periphery/buttons.c
@@ -2,23 +2,24 @@
 
 #include <pico/binary_info.h>
 
-static void button_init(uint gpio, gpio_irq_callback_t callback)
+static void button_init(uint gpio /*, gpio_irq_callback_t callback*/)
 {
-  gpio_set_dir(buttons_gpio_1, GPIO_IN);
-  gpio_set_pulls(buttons_gpio_1, true, false);
-  gpio_set_input_hysteresis_enabled(buttons_gpio_1, true);
-  gpio_set_irq_enabled_with_callback(buttons_gpio_1, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, callback);
+  gpio_init(gpio);
+  gpio_set_dir(gpio, GPIO_IN);
+  gpio_set_pulls(gpio, true, false);
+  gpio_set_input_hysteresis_enabled(gpio, true);
+  // gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, callback);
 }
 
-void buttons_init(gpio_irq_callback_t callback)
+void buttons_init(/*gpio_irq_callback_t callback*/)
 {
   // clang-format off
   bi_decl_if_func_used(bi_program_feature("User Buttons"))
   bi_decl_if_func_used(bi_2pins_with_names(buttons_gpio_1, "Button 1", buttons_gpio_2, "Button 2"))
     // clang-format on
 
-    button_init(buttons_gpio_1, callback);
-  button_init(buttons_gpio_2, callback);
+    button_init(buttons_gpio_1 /*, callback*/);
+  button_init(buttons_gpio_2 /*, callback*/);
 }
 
 bool buttons_is_pressed(uint button_gpio) { return !gpio_get(button_gpio); }
--- src/modules/periphery/buttons.h
+++ src/modules/periphery/buttons.h
@@ -3,15 +3,15 @@
 #include <hardware/gpio.h>
 #include <pico/types.h>
 
-constexpr uint buttons_gpio_1 = {15};
-constexpr uint buttons_gpio_2 = {14};
+constexpr uint buttons_gpio_1 = { 15 };
+constexpr uint buttons_gpio_2 = { 14 };
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-void buttons_init(gpio_irq_callback_t callback);
+void buttons_init(/*gpio_irq_callback_t callback*/);
 
 bool buttons_is_pressed(uint button_gpio);
 
