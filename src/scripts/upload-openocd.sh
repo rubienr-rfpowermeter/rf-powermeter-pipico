bdf42770e5f86b9a125a07965551e970e76a139f
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-10-29 00:12:25 +0100

    updates project from 2040 to 2350

--- src/scripts/upload-openocd.sh
+++ src/scripts/upload-openocd.sh
@@ -2,11 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROGRAM="rf_probe.elf"
+#BOARD="rp2040"
+BOARD="rp2350"
+
 
 pushd ${SCRIPT_DIR}/../build \
 && time \
 openocd --debug=2 \
-        --search  ${SCRIPT_DIR}/openocd \
-        --file rp2040-cmsis-dap.cfg \
+        --search ${SCRIPT_DIR}/openocd \
+        --file ${BOARD}-cmsis-dap.cfg \
         --command "program ${PROGRAM} verify reset exit" \
 && popd
