e4d43a5de402549e48e760d34cb3fcc35ec940b0
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-10-30 22:43:08 +0100

    cleanup TransactionData.h

--- src/lib/sample_data/TransactionData.h
+++ src/lib/sample_data/TransactionData.h
@@ -1,15 +1,9 @@
 #pragma once
 
-#pragma once
-
 #include "lib/ad7887/registers.h"
-#include "lib/ad8318/Converter3rdOrder.h"
-#include "lib/ad8318/KValues3rdOrder.hpp"
 #include "lib/sample_data/Average.h"
-#include <stdint.h>
 
 using namespace ad7887;
-using namespace ad8318;
 
 struct Temperature
 {
@@ -21,24 +15,12 @@ struct Temperature
 
 struct Probe
 {
-  /*
-  ControlRegister ctlRegister{ .powerManagement0       = 1,
-                               .powerManagement1       = 0,
-                               .mustBeZero1            = 0,
-                               .channelSelect          = 0,
-                               .singleDualChanelSelect = 0,
-                               .onChipReference        = 1,
-                               .mustBeZero2            = 0,
-                               .dontCare               = 0 };
-  AD7887 device{ ctlRegister, chipSelectDigitalWrite, clkDigitalWrite, dataDigitalWrite, dataDigitalRead, delayMicroseconds };
-  ad7887::SampleRegister sampleRegister{};
-  */
   Temperature temperature{};
 };
 
 struct TransactionData
 {
-  uint32_t timestamp_ms;
+  uint32_t timestamp_ms {0};
   Probe    probe{};
   Sample   raw_sample{};
 };
