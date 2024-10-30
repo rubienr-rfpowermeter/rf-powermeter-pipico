1e302f66b2f20308b8b6d3c19eb584eaf6dbf8de
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-10-30 22:23:14 +0100

    makes rp2350 default in dhe gdb-server.sh script

--- src/scripts/gdb-server.sh
+++ src/scripts/gdb-server.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+# Define this script as GDB-server command in "Embedded GDB server" run configuration (CLion).
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# MCU=rp2040
+MCU=rp2350
+
 pushd ${SCRIPT_DIR}/../build
 openocd --search  ${SCRIPT_DIR}/openocd \
-        --file rp2040-cmsis-dap.cfg
+        --file ${MCU}-cmsis-dap.cfg
 popd
