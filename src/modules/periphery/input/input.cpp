bb05635c637774342ce97e097e40c93027e637f7
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-10-30 01:02:56 +0100

    refactors input device: splits joystick (kbd key events) from hw buttons (keypad to screen point clicked event)

--- src/modules/periphery/input/input.cpp
+++ src/modules/periphery/input/input.cpp
@@ -10,9 +10,9 @@ void input_init() { irq_set_enabled(IO_IRQ_BANK0, true); }
 bool input_any_active(uint8_t mask, uint8_t state_flags) { return 0 != (mask & state_flags); }
 bool input_all_active(uint8_t mask, uint8_t state_flags) { return mask == (mask & state_flags); }
 
-TrackedInputState::TrackedInputState() : is_pressed(0), is_released(0), is_event(0), rfu(0), counter(0) { }
+TrackedInputState::TrackedInputState(uint8_t id) : is_pressed(0), is_released(0), is_event(0), rfu(0), counter(0), id(id) { }
 
-void TrackedInputState::update(bool signal)
+bool TrackedInputState::update(bool signal)
 {
   if(signal) counter = counter << 1 | 0x01;
   else counter >>= 1;
@@ -24,30 +24,34 @@ void TrackedInputState::update(bool signal)
 
   is_pressed  = is_now_pressed;
   is_released = is_now_released;
+
+  return is_event;
 }
 
 
-void TrackedInputs::update(uint8_t buttons_signal_mask, uint8_t joystick_signal_mask)
+void TrackedInputs::update_buttons(uint8_t buttons_signal_mask)
 {
-  {
-    a.update(input_any_active(BUTTONS_MASK_A, buttons_signal_mask));
-    b.update(input_any_active(BUTTONS_MASK_B, buttons_signal_mask));
-    x.update(input_any_active(BUTTONS_MASK_X, buttons_signal_mask));
-    y.update(input_any_active(BUTTONS_MASK_Y, buttons_signal_mask));
+  next_active_button = nullptr;
+  next_active_button = a.update(input_any_active(BUTTONS_MASK_A, buttons_signal_mask)) ? &a : next_active_button;
+  next_active_button = b.update(input_any_active(BUTTONS_MASK_B, buttons_signal_mask)) ? &b : next_active_button;
+  next_active_button = x.update(input_any_active(BUTTONS_MASK_X, buttons_signal_mask)) ? &x : next_active_button;
+  next_active_button = y.update(input_any_active(BUTTONS_MASK_Y, buttons_signal_mask)) ? &y : next_active_button;
 
-    button_event  = a.is_event | b.is_event | x.is_event | y.is_event;
-    button_active = a.is_pressed | b.is_pressed | x.is_pressed | y.is_pressed;
-  }
-  {
-    up.update(input_any_active(JOYSTICK_MASK_UP, joystick_signal_mask));
-    down.update(input_any_active(JOYSTICK_MASK_DOWN, joystick_signal_mask));
-    left.update(input_any_active(JOYSTICK_MASK_LEFT, joystick_signal_mask));
-    right.update(input_any_active(JOYSTICK_MASK_RIGHT, joystick_signal_mask));
-    z.update(input_any_active(JOYSTICK_MASK_Z, joystick_signal_mask));
+  button_event  = a.is_event + b.is_event + x.is_event + y.is_event;
+  button_active = a.is_pressed + b.is_pressed + x.is_pressed + y.is_pressed;
+}
 
-    joystick_event  = up.is_event | down.is_event | left.is_event | right.is_event | z.is_event;
-    joystick_active = up.is_pressed | down.is_pressed | left.is_pressed | right.is_pressed | z.is_pressed;
-  }
+void TrackedInputs::update_joystick(uint8_t joystick_signal_mask)
+{
+  next_active_axis = nullptr;
+  next_active_axis = up.update(input_any_active(JOYSTICK_MASK_UP, joystick_signal_mask)) ? &up : next_active_axis;
+  next_active_axis = down.update(input_any_active(JOYSTICK_MASK_DOWN, joystick_signal_mask)) ? &down : next_active_axis;
+  next_active_axis = left.update(input_any_active(JOYSTICK_MASK_LEFT, joystick_signal_mask)) ? &left : next_active_axis;
+  next_active_axis = right.update(input_any_active(JOYSTICK_MASK_RIGHT, joystick_signal_mask)) ? &right : next_active_axis;
+  next_active_axis = z.update(input_any_active(JOYSTICK_MASK_Z, joystick_signal_mask)) ? &z : next_active_axis;
+
+  joystick_event  = up.is_event + down.is_event + left.is_event + right.is_event + z.is_event;
+  joystick_active = up.is_pressed + down.is_pressed + left.is_pressed + right.is_pressed + z.is_pressed;
 }
 
 void TrackedInputs::print() const
--- src/modules/periphery/input/input.h
+++ src/modules/periphery/input/input.h
@@ -1,38 +1,43 @@
 #pragma once
 
-#include <pico/types.h>
+#include <cinttypes>
 
 typedef struct TrackedInputState
 {
   uint8_t is_pressed : 1;
   uint8_t is_released : 1;
   uint8_t is_event : 1;
-  uint8_t rfu : 3; // reserved
+  uint8_t rfu : 3;
   uint8_t counter;
+  uint8_t id;
 
-  void update(bool signal);
-  TrackedInputState();
+  bool update(bool signal);
+  TrackedInputState(uint8_t id);
 
 } TrackedInputState;
 
 typedef struct TrackedInputs
 {
-  TrackedInputState a;
-  TrackedInputState b;
-  TrackedInputState x;
-  TrackedInputState y;
-  TrackedInputState up;
-  TrackedInputState down;
-  TrackedInputState left;
-  TrackedInputState right;
-  TrackedInputState z;
+  TrackedInputState a{ 0x00 };     /// KEY_NEXT
+  TrackedInputState b{ 0x01 };     /// KEY_PREV
+  TrackedInputState x{ 0x02 };     /// KEY_HOME
+  TrackedInputState y{ 0x03 };     /// KEY_END
+  TrackedInputState up{ 0x11 };    /// KEY_UP
+  TrackedInputState down{ 0x12 };  /// KEY_DOWN
+  TrackedInputState left{ 0x13 };  /// KEY_RIGHT
+  TrackedInputState right{ 0x14 }; /// KEY_LEFT
+  TrackedInputState z{ 0x0a };     /// KEY_ENTER
 
-  bool joystick_active;
-  bool joystick_event;
-  bool button_active;
-  bool button_event;
+  uint8_t joystick_active;
+  uint8_t joystick_event;
+  uint8_t button_active;
+  uint8_t button_event;
 
-  void update(uint8_t buttons_signal_mask, uint8_t joystick_signal_mask);
+  TrackedInputState *next_active_button;
+  TrackedInputState *next_active_axis;
+
+  void update_buttons(uint8_t buttons_signal_mask);
+  void update_joystick(uint8_t joystick_signal_mask);
   void print() const;
 
 } TrackedInputs;
