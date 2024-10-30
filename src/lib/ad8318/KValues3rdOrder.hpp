9a96af22207049fbba3ae0055d0ddc59902637d3
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-10-30 21:38:55 +0100

    set default of KValues3rdOrder to "no correction"

--- src/lib/ad8318/KValues3rdOrder.hpp
+++ src/lib/ad8318/KValues3rdOrder.hpp
@@ -5,10 +5,10 @@ namespace ad8318
 
 template <typename T> struct KValues3rdOrder
 {
-  T k0{ 0 };
-  T k1{ 0 };
-  T k2{ 0 };
-  T k3{ 0 };
+  T k0{ 1 }; /// default: no correction
+  T k1{ 0 }; /// default: no correction
+  T k2{ 0 }; /// default: no correction
+  T k3{ 0 }; /// default: no correction
 };
 
 }   // namespace ad8318
