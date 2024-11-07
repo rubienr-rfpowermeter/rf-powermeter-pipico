97e258885031dd06059ba2265ec8312a39c54be1
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-11-07 01:44:43 +0100

    all KValues3rdOrder coefficients default now to 0

--- src/modules/rf_power_meter/KValues3rdOrder.hpp
+++ src/modules/rf_power_meter/KValues3rdOrder.hpp
@@ -5,7 +5,7 @@ namespace rfpm
 
 template <typename T> struct KValues3rdOrder
 {
-  T k0{ 1 };   /// default: no correction
+  T k0{ 0 };   /// default: no correction
   T k1{ 0 };   /// default: no correction
   T k2{ 0 };   /// default: no correction
   T k3{ 0 };   /// default: no correction
