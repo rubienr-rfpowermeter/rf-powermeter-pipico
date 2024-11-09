51783d36d6a30ba498f1d57d75d70f50a0499e96
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-11-09 20:25:09 +0100

    simplifies cmake/make scripts

--- src/scripts/cmake.sh
+++ src/scripts/cmake.sh
@@ -3,12 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd ${SCRIPT_DIR}/.. \
-&& mkdir build \
-&& pushd build \
-&& time cmake --no-warn-unused-cli \
-        -DPICO_OPTIMIZED_DEBUG=1 \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
-        -DCMAKE_BUILD_TYPE:STRING=Debug \
-        ../ \
-&& popd \
+&& rm -drf build \
+&& cmake --no-warn-unused-cli \
+         -DPICO_OPTIMIZED_DEBUG=1 \
+         -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+         -DCMAKE_BUILD_TYPE:STRING=Debug \
+         -S ./ -B ./build \
 && popd
--- src/scripts/make.sh
+++ src/scripts/make.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NUM_CORES=`nproc`
 
-pushd ${SCRIPT_DIR}/../build \
-&& time make -j $NUM_CORES \
+pushd ${SCRIPT_DIR}/.. \
+&& cmake --build ./build  -j $NUM_CORES \
 && popd
