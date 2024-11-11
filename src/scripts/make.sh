ea48833e35678f8f4639855eec497ba55c7aa532
Raoul Rubien <rubienr@sbox.tugraz.at>
2024-11-11 11:11:50 +0100

    simplifies make script; now -j vs -v `nproc`

--- src/scripts/make.sh
+++ src/scripts/make.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NUM_CORES=`nproc`
 
 pushd ${SCRIPT_DIR}/.. \
-&& cmake --build ./build  -j $NUM_CORES \
+&& cmake --build ./build  -j \
 && popd
