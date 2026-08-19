b2c115503295940575e18c34a744be91137dfcb2
Raoul Rubien <rubienr@sbox.tugraz.at>
2026-08-19 21:43:58 +0200

    cmake.sh: uses Ninja generator, removes deprecated define

--- src/scripts/cmake.sh
+++ src/scripts/cmake.sh
@@ -4,8 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd ${SCRIPT_DIR}/.. \
 && rm -drf build \
-&& cmake --no-warn-unused-cli \
-         -DPICO_OPTIMIZED_DEBUG=1 \
+&& cmake -GNinja \
          -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
          -DCMAKE_BUILD_TYPE:STRING=Debug \
          -S ./ -B ./build \
